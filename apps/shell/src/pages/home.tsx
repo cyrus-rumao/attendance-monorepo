5d48d889b249f49fe7a1d4ee51cdb2e83272d00e
Cyrus Rumao <cyrus.rumao26@gmail.com>
2026-03-29 13:47:26 +0530

    test commit

--- apps/shell/src/pages/home.tsx
+++ apps/shell/src/pages/home.tsx
@@ -57,7 +57,7 @@ const Home: React.FC = () => {
             <div>
               <div className="inline-block mb-4">
                 <span className="text-amber-500 text-sm font-medium tracking-wider uppercase">
-         This is smart nonsense
+         smart attendance tracking
                 </span>
               </div>
 
@@ -65,7 +65,7 @@ const Home: React.FC = () => {
                 Never Miss
                 <br />
                 <span className="text-transparent bg-clip-text bg-linear-to-r from-amber-500 to-yellow-600">
-                  Your Death
+                  Your Class
                 </span>
                 <br />
                 Again
